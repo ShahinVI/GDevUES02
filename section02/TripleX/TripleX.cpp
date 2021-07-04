eedd6a6bf7d0cf3845a97e8b2f1f33b3f0cd3634
ShahinVI <shahinaiman@gmail.com>
2021-07-04 17:09:49 +0200

    first commit

---
+++ section02/TripleX/TripleX.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "		you are a lazy ass dude that is trying to save the world\n";
+	std::cout << "		with your lazy plan of not doing anything, the bad guys \n";
+	std::cout << "		are not aware of you of your existience like your crush.\n";
+	std::cout << "	    Therefore you were perfectly able to sneak to the server room!\n\n";
+	std::cout << "			Now enter the code to avoid ARMAGEDON!\n";
+	std::cout << "					Code:	";
+
+	return 0;
+
+}
